bd84111201be10ce8ac083dc412914d7f0ab4c2e
Nathan Brooks <natebrooks004@gmail.com>
2022-08-08 10:41:38 -0700

    docs: comment alternative

--- problems/longest_substring_without_repeating_characters.py
+++ problems/longest_substring_without_repeating_characters.py
@@ -2,6 +2,7 @@ def length_of_longest_substring(s: str) -> int:
     i = j = 0
     length = 0
     while j < len(s):
+        # An O(1) operation would introduce additional overhead here
         while s.find(s[j], i, j) != -1:
             i += 1
         j += 1
