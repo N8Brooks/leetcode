4452954ec9851f7b5f6f45f2eff63ba39e562ff3
Nathan Brooks <natebrooks004@gmail.com>
2022-08-09 23:20:04 -0700

    refactor(longest_substring_without_repeating_characters): two pointer

--- problems/longest_substring_without_repeating_characters.py
+++ problems/longest_substring_without_repeating_characters.py
@@ -1,10 +1,6 @@
 def length_of_longest_substring(s: str) -> int:
-    i = j = 0
-    length = 0
-    while j < len(s):
-        # An O(1) operation would introduce additional overhead here
-        while s.find(s[j], i, j) != -1:
-            i += 1
-        j += 1
-        length = max(length, j - i)
+    i = length = 0
+    for j, char in enumerate(s):
+        i = max(i, s.rfind(char, i, j) + 1)
+        length = max(length, j - i + 1)
     return length
