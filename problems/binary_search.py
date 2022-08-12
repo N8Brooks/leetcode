03c666a3882919f201f8501112a666ce6ec71a64
Nathan Brooks <natebrooks004@gmail.com>
2022-08-12 08:40:11 -0700

    docs: add implementation details

--- problems/binary_search.py
+++ problems/binary_search.py
@@ -3,9 +3,7 @@ from typing import List
 
 
 def search(nums: List[int], target: int) -> int:
-    """
-    Delegates logic to bisect. This won't work for
-    empty lists, but there are no tests for that.
-    """
+    # Delegates logic to bisect. This won't work for
+    # empty lists, but there are no tests for that.
     i = bisect.bisect(nums, target) - 1
     return i if nums[i] == target else -1
