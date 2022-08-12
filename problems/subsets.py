abd3a295fec3b4e14a7fd2e1ac8cbc41b59224a2
Nathan Brooks <natebrooks004@gmail.com>
2022-08-12 08:38:24 -0700

    docs: add implementation details

--- problems/subsets.py
+++ problems/subsets.py
@@ -3,4 +3,5 @@ from typing import List
 
 
 def subsets(nums: List[int]) -> List[List[int]]:
+    # Implements the power_set from the itertools recipes
     return chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1))
