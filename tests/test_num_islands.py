2eb31a9d2e8bc51ee5f8ad03acc3c609d908f2c5
Nathan Brooks <natebrooks004@gmail.com>
2022-08-12 18:44:43 -0700

    feat(num_islands): num_islands

---
+++ tests/test_num_islands.py
@@ -0,0 +1,33 @@
+from problems.num_islands import num_islands
+
+
+def test_example_1():
+    assert (
+        num_islands(
+            [
+                ["1", "1", "1", "1", "0"],
+                ["1", "1", "0", "1", "0"],
+                ["1", "1", "0", "0", "0"],
+                ["0", "0", "0", "0", "0"],
+            ]
+        )
+        == 1
+    )
+
+
+def test_example_2():
+    assert (
+        num_islands(
+            [
+                ["1", "1", "0", "0", "0"],
+                ["1", "1", "0", "0", "0"],
+                ["0", "0", "1", "0", "0"],
+                ["0", "0", "0", "1", "1"],
+            ]
+        )
+        == 3
+    )
+
+
+def test_one():
+    assert num_islands([["1"]]) == 1
