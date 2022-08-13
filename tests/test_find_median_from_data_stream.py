d091ac059b96c479c74e0e87774e0003e83f2745
Nathan Brooks <natebrooks004@gmail.com>
2022-08-12 19:22:49 -0700

    feat(find_median_from_data_stream): MedianFinder

---
+++ tests/test_find_median_from_data_stream.py
@@ -0,0 +1,25 @@
+from problems.find_median_from_data_stream import MedianFinder
+
+
+def assert_input_correct(instructions, values):
+    finder: MedianFinder
+    for instruction, value in zip(instructions, values):
+        if instruction == "MedianFinder":
+            finder = MedianFinder()
+        elif instruction == "addNum":
+            finder.add_num(value[0])
+        elif instruction == "findMedian":
+            assert finder.find_median() == value[0]
+        else:
+            raise ValueError(f"{instruction} is not a valid instruction")
+
+
+def test_example_1():
+    assert_input_correct(
+        ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"],
+        [[], [1], [2], [1.5], [3], [2]],
+    )
+
+
+def test_one():
+    assert_input_correct(["MedianFinder", "addNum", "findMedian"], [[], [1], [1]])
