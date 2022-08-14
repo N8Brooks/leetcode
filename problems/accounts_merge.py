3346565d0c0893dfa83b1ad7fa7b293f4e657d8c
Nathan Brooks <natebrooks004@gmail.com>
2022-08-13 22:52:54 -0700

    feat(accounts_merge): accounts_merge

---
+++ problems/accounts_merge.py
@@ -0,0 +1,22 @@
+from bisect import insort
+from typing import List
+
+
+def accounts_merge(accounts: List[List[str]]) -> List[List[str]]:
+    def find(email):
+        return email if email == parent.get(email, email) else find(parent[email])
+
+    parent = {}
+    lookup = {}
+    for name, *emails in accounts:
+        zero = find(emails[0])
+        for email in emails:
+            parent[find(email)] = zero
+            lookup[email] = name
+    groups = {}
+    for email in parent:
+        name = lookup[email]
+        zero = find(email)
+        emails = groups.setdefault(zero, [name])
+        insort(emails, email, 1)
+    return list(groups.values())
