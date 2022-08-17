eb52d099f97abe27dfc3610738064731535a5e55
Nathan Brooks <natebrooks004@gmail.com>
2022-08-17 14:05:09 -0700

    refactor(binary_search): works for empty nums

--- problems/binary_search.py
+++ problems/binary_search.py
@@ -3,7 +3,5 @@ from typing import List
 
 
 def search(nums: List[int], target: int) -> int:
-    # Delegates logic to bisect. This won't work for
-    # empty lists, but there are no tests for that.
     i = bisect.bisect(nums, target) - 1
-    return i if nums[i] == target else -1
+    return i if nums and nums[i] == target else -1
