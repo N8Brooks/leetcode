c77cfc1c8e22d390e18be3f3a66c3712c71e3dc2
Nathan Brooks <natebrooks004@gmail.com>
2022-08-17 13:20:03 -0700

    refactor(string_to_integer_atoi): prefer less constants

--- problems/string_to_integer_atoi.py
+++ problems/string_to_integer_atoi.py
@@ -2,11 +2,7 @@ import re
 
 PATTERN = re.compile(r"\s*([-+]?\d+)")
 
-LO = -(2**31)
-
-HI = 2**31 - 1
-
 
 def my_atoi(s: str) -> int:
     match = PATTERN.match(s)
-    return max(LO, min(HI, int(match[0]))) if match else 0
+    return max(-(2**31), min(2**31 - 1, int(match[0]))) if match else 0
