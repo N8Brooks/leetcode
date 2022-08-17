112d41adfa8a7e37212f6b5e7362028d6a66bc62
Nathan Brooks <natebrooks004@gmail.com>
2022-08-17 14:56:30 -0700

    refactor(binary_matrix): update approach

--- problems/binary_matrix.py
+++ problems/binary_matrix.py
@@ -5,22 +5,16 @@ def update_matrix(mat: List[List[int]]) -> List[List[int]]:
     flooded = {
         (x, y) for y, row in enumerate(mat) for x, bit in enumerate(row) if not bit
     }
-    res = [[None for _ in row] for row in mat]
-    width = len(mat[0])
-    height = len(mat)
+    result = [[None] * len(mat[0]) for _ in mat]
     distance = 0
     while flooded:
-        next_flooded = set()
         for x, y in flooded:
-            res[y][x] = distance
-            if x - 1 >= 0 and res[y][x - 1] is None:
-                next_flooded.add((x - 1, y))
-            if x + 1 < width and res[y][x + 1] is None:
-                next_flooded.add((x + 1, y))
-            if y - 1 >= 0 and res[y - 1][x] is None:
-                next_flooded.add((x, y - 1))
-            if y + 1 < height and res[y + 1][x] is None:
-                next_flooded.add((x, y + 1))
+            result[y][x] = distance
+        flooded = {
+            (x, y)
+            for x, y in flooded
+            for x, y in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1))
+            if 0 <= x < len(mat[0]) and 0 <= y < len(mat) and result[y][x] is None
+        }
         distance += 1
-        flooded = next_flooded - flooded
-    return res
+    return result
