202b561dbe4c3ee8c86a967a1516e6c548def7ef
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 14:50:02 -0700

    refactor(maximum_depth_of_a_binary_tree): iterative solution

--- problems/maximum_depth_of_a_binary_tree.py
+++ problems/maximum_depth_of_a_binary_tree.py
@@ -4,11 +4,9 @@ from definitions.tree_node import TreeNode
 
 
 def max_depth(root: Optional[TreeNode]) -> int:
-    def parse(node: Optional[TreeNode]) -> int:
-        if not node:
-            return 0
-        ldepth = parse(node.left)
-        rdepth = parse(node.right)
-        return max(ldepth, rdepth) + 1
-
-    return parse(root)
+    row = [root] if root else []
+    level = 0
+    while row:
+        row = [node for node in row for node in (node.left, node.right) if node]
+        level += 1
+    return level
