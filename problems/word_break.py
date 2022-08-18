ceb8618c855569292b99b07974c1b359d4d98d4f
skookumchoocher <natebrooks004@gmail.com>
2022-08-18 15:15:45 -0700

    refactor(word_break): add types

--- problems/word_break.py
+++ problems/word_break.py
@@ -1,9 +1,12 @@
-def word_break(s, words):
+from typing import List
+
+
+def word_break(s: str, word_dict: List(str)) -> bool:
     breaks = [True] + [False] * len(s)
     for i, is_break in enumerate(breaks):
         if not is_break:
             continue
-        for word in words:
+        for word in word_dict:
             if s[i : i + len(word)] == word:
                 breaks[i + len(word)] = True
     return breaks[-1]
