5650639fb9bec1bdd06e3e9c6f70258b527ee3d4
skookumchoocher <natebrooks004@gmail.com>
2022-08-18 15:18:03 -0700

    fix(word_break): use bracket notation for types

--- problems/word_break.py
+++ problems/word_break.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def word_break(s: str, word_dict: List(str)) -> bool:
+def word_break(s: str, word_dict: List[str]) -> bool:
     breaks = [True] + [False] * len(s)
     for i, is_break in enumerate(breaks):
         if is_break:
