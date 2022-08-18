b917ae7aa5192e3e4be16fe7c1c9dc69553cab58
Nathan Brooks <natebrooks004@gmail.com>
2022-08-17 20:17:05 -0700

    refactor(flood_fill): update instead of multiple add

--- problems/flood_fill.py
+++ problems/flood_fill.py
@@ -11,8 +11,12 @@ def flood_fill(image: List[List[int]], sr: int, sc: int, color: int) -> List[Lis
         r, c = fill.pop()
         if 0 <= c < len(image[0]) and 0 <= r < len(image) and image[r][c] == start:
             image[r][c] = color
-            fill.add((r - 1, c))
-            fill.add((r + 1, c))
-            fill.add((r, c - 1))
-            fill.add((r, c + 1))
+            fill.update(
+                (
+                    (r - 1, c),
+                    (r + 1, c),
+                    (r, c - 1),
+                    (r, c + 1),
+                )
+            )
     return image
