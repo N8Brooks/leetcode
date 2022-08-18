6d0aea141b5c5845ad58242d21f379490edb18aa
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 14:01:01 -0700

    refactor(invert_binary_tree): simply none checks

--- problems/invert_binary_tree.py
+++ problems/invert_binary_tree.py
@@ -4,12 +4,10 @@ from definitions.tree_node import TreeNode
 
 
 def invert_tree(root: Optional[TreeNode]) -> Optional[TreeNode]:
-    stack = [root] if root else []
+    stack = [root]
     while stack:
         node = stack.pop()
-        node.left, node.right = node.right, node.left
-        if node.left:
-            stack.append(node.left)
-        if node.right:
-            stack.append(node.right)
+        if node:
+            node.left, node.right = node.right, node.left
+            stack.extend((node.left, node.right))
     return root
