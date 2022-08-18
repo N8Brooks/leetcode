bbd9122b9a5dafce3161f32a3a235c940c5a3fc2
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 14:04:31 -0700

    refactor(climbing_stairs): remove sqrt 5

--- problems/climbing_stairs.py
+++ problems/climbing_stairs.py
@@ -1,8 +1,6 @@
 PHI = (5**0.5 + 1) / 2
 
-SQRT_5 = 5**0.5
-
 
 def climb_stairs(n: int) -> int:
     # Based on O(1) fibonacci solution
-    return round(PHI ** (n + 1) / SQRT_5)
+    return round(PHI ** (n + 1) / 5**0.5)
