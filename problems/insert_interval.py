799d4c27c3f0b27b13baf895b3482e307fdb76da
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 13:58:53 -0700

    style(insert_interval): ignore black formatting

--- problems/insert_interval.py
+++ problems/insert_interval.py
@@ -2,15 +2,12 @@ import bisect
 from math import inf
 from typing import List
 
+# fmt: off
 
 def insert(intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
-    i = bisect.bisect_left(
-        intervals, True, key=lambda interval: interval[1] >= new_interval[0]
-    )
+    i = bisect.bisect_left(intervals, True, key=lambda interval: interval[1] >= new_interval[0])
     j = bisect.bisect_left(intervals, [new_interval[1], inf], lo=i)
-    left = (
-        min(intervals[i][0], new_interval[0]) if i < len(intervals) else new_interval[0]
-    )
+    left = (min(intervals[i][0], new_interval[0]) if i < len(intervals) else new_interval[0])
     right = max(intervals[j - 1][1], new_interval[1]) if j > 0 else new_interval[1]
     intervals[i:j] = [[left, right]]
     return intervals
