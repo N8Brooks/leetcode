f545bb6036843ca50b4667aa4779b58b10950605
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 14:36:39 -0700

    refactor(subsets): break up implementation

--- problems/subsets.py
+++ problems/subsets.py
@@ -1,7 +1,11 @@
 from itertools import chain, combinations
-from typing import List
+from typing import Generator, List, Tuple
+
+
+def powerset(nums: List[int]) -> Generator[Tuple[int], None, None]:
+    """Defined in itertools documentation recipes"""
+    yield from chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1))
 
 
 def subsets(nums: List[int]) -> List[List[int]]:
-    # Implements the power_set from the itertools recipes
-    return chain.from_iterable(combinations(nums, r) for r in range(len(nums) + 1))  # type: ignore
+    return [list(combination) for combination in powerset(nums)]
