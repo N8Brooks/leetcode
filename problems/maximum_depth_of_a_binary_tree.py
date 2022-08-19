229557108e6d754e747f76260d0851f705d3d742
Nathan Brooks <natebrooks004@gmail.com>
2022-08-18 17:59:45 -0700

    refactor(maximum_depth_of_a_binary_tree): move depth

--- problems/maximum_depth_of_a_binary_tree.py
+++ problems/maximum_depth_of_a_binary_tree.py
@@ -4,9 +4,9 @@ from definitions.tree_node import TreeNode
 
 
 def max_depth(root: Optional[TreeNode]) -> int:
-    row = [root] if root else []
     depth = 0
-    while row:
-        row = [node for node in row for node in (node.left, node.right) if node]
+    level = [root] if root else []
+    while level:
         depth += 1
+        level = [node for node in level for node in (node.left, node.right) if node]
     return depth
