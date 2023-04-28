212dd8f26c95b302f9d689a349f906a64b66cc4c
N8Brooks <natebrooks004@gmail.com>
2023-04-28 13:10:51 -0500

    feat(javascript): array reduce transform

---
+++ javascript/javascript/array_reduce_transform.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+  for (const num of nums) {
+    init = fn(init, num);
+  }
+  return init;
+};
+
+it("Case 1", () => {
+  expect(reduce([1, 2, 3, 4], (accum, curr) => accum + curr, 0)).toBe(10);
+});
+
+it("Case 2", () => {
+  expect(reduce([1, 2, 3, 4], (accum, curr) => accum + curr * curr, 100)).toBe(
+    130
+  );
+});
+
+it("Case 3", () => {
+  expect(reduce([], (accum, curr) => accum + curr, 25)).toBe(25);
+});
